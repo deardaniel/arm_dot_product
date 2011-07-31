f19381e469b9dac0434e8c9734c8a23a30824e26
Daniel Heffernan <daniel@bearface.local>
2011-08-01 02:51:18 +0900

    Renamed test.s to dot_product.s. Added ARMv6 and ARMv7 (NEON) targets/products. Added NEON-based floating point inner product calculation and testing. Improved testing to check ASM results against C reference implementation.

--- ARM/ARMAppDelegate.m
+++ ARM/ARMAppDelegate.m
@@ -16,7 +16,48 @@
 @synthesize window = _window;
 @synthesize viewController = _viewController;
 
-int test(int32_t *,int32_t *, int32_t);
+void test_int32(int32_t *,int32_t *, int32_t);
+int32_t dot_product_int32(int32_t *,int32_t *, int32_t);
+int32_t dot_product_int32_asm(int32_t *,int32_t *, int32_t);
+
+void test_int32(int32_t *x,int32_t *y, int32_t n) {
+    int32_t cResult = dot_product_int32(x, y, n);
+    int32_t asmResult = dot_product_int32_asm(x, y, n);
+    printf("%d => %d :: %d (%s)\n",
+           n,
+           cResult,
+           asmResult,
+           cResult == asmResult ? "SUCCESS" : "FAIL");
+}
+
+int32_t dot_product_int32(int32_t *x,int32_t *y, int32_t n) {
+    int32_t sum = 0;
+    while (--n >= 0) sum += x[n] * y[n];
+    return sum;
+}
+
+#ifdef __ARM_NEON__
+void test_float(float *,float *, int32_t);
+float dot_product_float(float *,float *, int32_t);
+float dot_product_float_asm(float *,float *, int32_t);
+
+void test_float(float *x,float *y, int32_t n) {
+    float cResult = dot_product_float(x, y, n);
+    float asmResult = dot_product_float_asm(x, y, n);
+    printf("%d => %f :: %f (%s)\n",
+           n,
+           cResult,
+           asmResult,
+           cResult == asmResult ? "SUCCESS" : "FAIL");
+}
+
+float dot_product_float(float *x,float *y, int32_t n) {
+    float sum = 0.0f;
+    while (--n >= 0) sum += x[n] * y[n];
+    return sum;
+}
+#endif
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -25,21 +66,31 @@ int test(int32_t *,int32_t *, int32_t);
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
 
-    int32_t x[] = { 10, 20, 30, 40, 2 };
-    int32_t y[] = { 10, 9, 8, 7, 4 };
-    
-    int32_t res = test(x, y, 5);
-    NSLog(@"Result (5): %d", res);
-
-    res = test(x, y, 4);
-    NSLog(@"Result (4): %d", res);
-    
-    res = test(x, y, 3);
-    NSLog(@"Result (3): %d", res);
-    
-    res = test(x, y, 0);
-    NSLog(@"Result (0): %d", res);
-
+    {
+        int32_t x[] = { 10, 20, 30, 40,
+                        50, 60, 70, 80,
+                        90, 100 };
+        int32_t y[] = { 10, 9, 8, 7,
+                        6, 5, 4, 3,
+                        2, 1 };
+        
+        for (int i=0; i<=10; i++)
+            test_int32(x, y, i);
+    }
+#ifdef __ARM_NEON__
+    {
+        float x[] = { 10.0f, 20.0f, 30.0f, 40.0f,
+                        50.0f, 60.0f, 70.0f, 80.0f,
+                        90.0f, 100.0f };
+        float y[] = { 10.0f, 9.0f, 8.0f, 7.0f,
+                        6.0f, 5.0f, 4.0f, 3.0f,
+                        2.0f, 1.0f };
+        
+        for (int i=0; i<=10; i++)
+            test_float(x, y, i);
+        
+    }
+#endif
     return YES;
 }
 
---
+++ ARM/dot_product.s
@@ -0,0 +1,121 @@
+//
+//  Test.s
+//  ARM
+//
+//  Created by Daniel Heffernan on 30/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifdef __arm__
+
+.text
+.align 4
+.globl _dot_product_int32_asm
+
+_dot_product_int32_asm:
+    push {r4-r6,lr}
+    mov r6,#0
+
+    cmp r2, #0
+    beq .Lend
+
+#ifdef __ARM_NEON__
+
+    cmp r2, #8
+    blt .Lsingle
+
+    vmov.s32 q0,#0
+
+    .Lbatchloop:
+
+        vld1.s32 {q1-q2},[r0]!
+        vld1.s32 {q3-q4},[r1]!
+
+        vmul.s32 q1, q1, q3
+        vmul.s32 q3, q2, q4
+
+        vpadd.s32 d1, d2, d3
+        vpadd.s32 d6, d7, d6
+        vpadd.s32 d1, d1, d6
+
+        vpadd.s32 d0, d0, d1
+        subs r2, r2, #8
+        cmp r2, #8
+        bge .Lbatchloop
+
+    vpadd.s32 d0, d0, d0
+    vmov.s32 r6, s0
+
+    cmp r2, #0
+    beq .Lend
+
+#endif
+
+    .Lsingle:
+        .Lloop:
+            ldr r4,[r0],#4
+            ldr r5,[r1],#4
+
+            mul r4,r4,r5
+            add r6,r6,r4
+
+            subs r2,r2,#1
+            bne .Lloop
+
+
+    .Lend:
+        mov r0, r6
+        pop {r4-r6,lr}
+        mov pc,lr
+
+
+#ifdef __ARM_NEON__
+.globl _dot_product_float_asm
+_dot_product_float_asm:
+    vmov.f32 q0,#0.0
+
+    cmp r2, #0
+    beq .Lfend
+
+    cmp r2, #8
+    blt .Lfsingle
+
+    .Lfbatchloop:
+        vld1.f32 {q1-q2},[r0]!
+        vld1.f32 {q3-q4},[r1]!
+
+        vmul.f32 q1, q1, q3
+        vmul.f32 q3, q2, q4
+
+        vpadd.f32 d1, d2, d3
+        vpadd.f32 d6, d7, d6
+        vpadd.f32 d1, d1, d6
+
+        vpadd.f32 d0, d0, d1
+        subs r2, r2, #8
+        cmp r2, #8
+        bge .Lfbatchloop
+
+    vpadd.f32 d0, d0, d0
+
+    cmp r2, #0
+    beq .Lfend
+
+    .Lfsingle:
+        .Lfloop:
+            vldm r0!, {s1}
+            vldm r1!, {s2}
+
+            vmul.f32 s1,s1,s2
+            vadd.f32 s0,s0,s1
+
+            subs r2,r2,#1
+            bne .Lfloop
+
+    .Lfend:
+        vmov r0, s0
+
+        mov pc,lr
+#endif
+
+#endif
